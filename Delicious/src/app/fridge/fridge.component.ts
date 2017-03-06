7f65f85a33286b2ccd6a71ea432795c302033a35
henyy <henriima@metropolia.fi>
2017-03-06 14:54:25 +0200

    edited fridge component

--- Delicious/src/app/fridge/fridge.component.ts
+++ Delicious/src/app/fridge/fridge.component.ts
@@ -15,24 +15,35 @@ export class FridgeComponent implements OnInit {
   @ViewChild(RecipeComponent)
   private resipecomponent: RecipeComponent;
 
-  private ingredient: string = '';
+  ingredients: any = [{id: 'ingredient1'}, {id: 'ingredient2'}];
+
+  private ingredient1: string = '';
   private ingredient2: string = '';
   private ingredient3: string = '';
   private ingredient4: string = '';
   private ingredient5: string = '';
   private ingredient6: string = '';
 
-
   constructor(private  foodService: FoodService, private router: Router) {
   }
 
+  addNewItem = () =>{
+    var newItemNo = this.ingredients.length + 1;
+    this.ingredients.push({'id': 'ingredient'+newItemNo});
+    console.log(this.ingredients);
+  };
+
+  removeItem = () =>{
+    var lastItem = this.ingredients.length - 1;
+    this.ingredients.splice(lastItem);
+    console.log(this.ingredients);
+  };
 
   getFridgefood = () => {
 
-    this.foodService.setIngredients(`${this.ingredient}, ${this.ingredient2}, ${this.ingredient3}, ${this.ingredient4}, ${this.ingredient5}, ${this.ingredient6}`);
+    this.foodService.setIngredients(`${this.ingredients[0]}, ${this.ingredient2}, ${this.ingredient3}, ${this.ingredient4}, ${this.ingredient5}, ${this.ingredient6}`);
     this.resipecomponent.getRecipe('aine');
   };
-
 
   ngOnInit() {
   }
